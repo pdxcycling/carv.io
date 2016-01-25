5760fdd5ca49d6ba0f49d01f2ae356cbf4d4265b
fiannacci <francis.c.iannacci@gmail.com>
2016-01-24 17:51:29 -0800

    minor cleanup of audio code

--- audio_analysis/code/audio_analysis.py
+++ audio_analysis/code/audio_analysis.py
@@ -1,31 +1,39 @@
-## Audio_Analysis
 class AudioAnalysis(object):
-    '''
+    """
+    Class for processing audio
+
     TODO:
     - Define standard interface for all methods
     - Define type of audio that will be passed in
-    - Check if the audio is music or not!
-    '''
+    - Check if the audio is music or not. This will rely on metadata.
+    """
+
     def __init__(self):
-        '''
+        """
         Default constructor
-        '''
+        """
         pass
 
-    ## Beat detection
     def extract_beat(audio):
+        """
+        Beat detection
+        """
         pass
 
-    ## Energy detection
     def extract_energy():
-        '''
-        '''
+        """
+        Detect spectral energy
+        """
         pass
 
-    ## Spectral Detection
     def extract_spectrum():
+        """
+        Get frequency spectrum
+        """
         pass
 
-    ## Loopability (vine-specific)
     def extract_loopability():
+        """
+        Checking for whether clip can be looped (Vine-specific)
+        """
         pass
--- audio_analysis/code/preprocess_audio.py
+++ audio_analysis/code/preprocess_audio.py
@@ -7,15 +7,19 @@ class Preprocess(object):
     TODO: Instantiate object or just treat these as static methods???
     TODO: Should both audio AND video have beat and frequency analysis???
     '''
-    ## Location where audio files will be stored
-    ## Will this still work when this is moved over to Amazon?
+    # Location where audio files will be stored
 
     @staticmethod
     def separate_audio(video_id, video_file_name):
-        '''
-        TODO: This should work on video files and database objects
-        Returns a string of audio file location
-        '''
+        """
+        Strip the audio from a video file
+
+        ARGS:
+            video_id: unique global video identifier
+            video_file_name: path to video file
+        RETURNS:
+            String to audio file location
+        """
         ## From http://stackoverflow.com/questions/26741116/python-extract-wav-from-video-file
         #_id = video_file_name.split("\.mp4")[0]
         _id = video_id
@@ -25,14 +29,3 @@ class Preprocess(object):
                   " -ab 160k -ac 2 -ar 44100 -vn " + wav_file_name
         subprocess.call(command, shell=True)
         return wav_file_name
-
-    # @staticmethod
-    # def _plot(self):
-    #     '''
-    #     TODO: Implement this
-    #     Useful to debugging, plotting
-    #     '''
-    #     rate, data = scipy.io.wavfile.read('audio.wav')
-    #     sp = np.fft.fft(data)
-    #     plt.plot(sp.real)
-    #     plt.show()
