4524c73de06e5d390c84d932e5a004600b738f0f
fiannacci <francis.c.iannacci@gmail.com>
2016-01-24 19:45:16 -0800

    cleaned vine code

--- data_mining/code/vine_scraper.py
+++ data_mining/code/vine_scraper.py
@@ -6,31 +6,43 @@ import json
 import urllib
 
 class VineScraper(object):
-    '''
-    Random notes:
-    Actually, scrape this url: https://vine.co/tags/snowboarding
-    Or this: https://vine.co/api/timelines/tags/snowboarding?page=5&anchor=1281492978820640768&size=20
-    '''
+    """
+    Main class for scraping data from Vine
+
+    Potential target URLs for scraping:
+    https://vine.co/tags/snowboarding
+    https://vine.co/api/timelines/tags/snowboarding?page=5&anchor=1281492978820640768&size=20
+    """
     ## Static class variables
-    ## TODO: what's the preferred style for variables like this?
     max_results_per_page = 20
 
     def __init__(self, rate_limit=0):
-        '''
+        """
         Default constructor
-        rate_limit: max requests per second
-        '''
+
+        ARGS:
+            rate_limit: max requests per second
+        RETURNS:
+            None
+        """
         self.dir = "./media"
         ## TODO: incorporate rate limiting in scraping requests
         self.rate_limit = rate_limit
         self.speed_throttle = speed_limit == 0
 
     def scrape(self, tags, num_records):
-        '''
-        DESCRIPTION:
-        INPUT:
-        OUTPUT:
-        '''
+        """
+        Scrapes Vine search page and downloads all videos matching
+        criteria.
+
+        TODO: Change from hard-coded search.
+
+        ARGS:
+            tags: hash tags to search for
+            num_records: number of records to return
+        RETURNS:
+            None
+        """
         ## TODO: tags input is currently unused. Do something with this.
         results = []
         page_size = VineScraper.max_results_per_page ## TODO: hard-coded for now...
@@ -47,19 +59,25 @@ class VineScraper(object):
             self._get_video_from_html(r_page)
 
     def _get_video_from_html(self, results_page, verbose=False):
-        '''
-        Finds video URL(s) in results page
-        Returns nothing... or should this be the number of videos successfully scraped?
-        '''
-        print results_page.text
+        """
+        Finds video URL(s) in results page, and downloads video locally.
+
+        ARGS:
+            results_page: HTML of Vine page
+            verbose: verbose output on
+        RETURNS:
+            None
+        """
         d = json.loads(results_page.text)
         for record in d['data']['records']:
             video_url = record['videoUrl']
             if verbose:
                 print "Video url: " + video_url
-            ## Download video
             self._download_from_url(video_url)
 
     def _download_from_url(self, url):
+        """
+        Download video from URL
+        """
         target_file_name = self.dir + "/" + url.split('/')[-1].split('?')[0]
         urllib.urlretrieve (url, target_file_name)
